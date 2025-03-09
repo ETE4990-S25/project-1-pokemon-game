1055fe78321298d220d892fa7b32d26a716033e4
David Long <dllong@cpp.edu>
2025-03-09 15:49:15 -0700

    Start if Menu function

--- Project 1/Menu.py
+++ Project 1/Menu.py
@@ -1,4 +1,13 @@
+import PokemonGame
 
 
-def Menu()
-    print()
+def Start():        ## Need to check save to see how many slots available
+    print("---Welcome to Pokemon---")
+    Name = input("What is your name?")
+    Gender = input("Are you a boy or a girl?")
+    Player1 = PokemonGame.Player(Name, Gender)
+
+
+def Menu():      ## Show what attacks available, option to run, check inventory, switch pokemon, Pokemon on field
+
+
