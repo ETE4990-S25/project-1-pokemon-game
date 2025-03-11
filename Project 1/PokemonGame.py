534d3d319bbf6ac2f7f6e2193e08238dd57ca07b
Jacob Juarez <jacobmjuarez24@gmail.com>
2025-03-11 03:34:06 -0700

    Added Items class, added item display in Player class, randomized TallGrass encounter

--- Project 1/PokemonGame.py
+++ Project 1/PokemonGame.py
@@ -1,3 +1,13 @@
+#inventory management (backpack)
+    #items that stack
+    #single use items
+    #multi use items
+#inventory display
+#item details
+#player selection
+#game play main function
+#save menu w/ json files (allow progress to be kept when file is closed)
+
 import random
 
 # Define Elements (powers for characters)
@@ -7,27 +17,79 @@ FIRE = "fire"
 GRASS = "grass"
 NORMAL = "normal"
 
-# Element Effectiveness against other elements
-Element_Effectivness = {
-    # Elemental Strengths (Amplifiers)
+# Element effectiveness against other elements
+element_effectiveness = {
+    # Elemental strengths (Amplifiers)
     (ELECTRIC, WATER): 1.4,  # Electric is strong against Water
     (WATER, FIRE): 1.4,      # Water is strong against Fire
     (FIRE, GRASS): 1.4,      # Fire is strong against Grass
     (GRASS, WATER): 1.4,     # Grass is strong against Water
 
-    # Elemental Weaknesses (Reducers)
+    # Elemental weaknesses (Reducers)
     (WATER, GRASS): 0.7,     # Water is weak against Grass
     (FIRE, WATER): 0.7,      # Fire is weak against Water
     (GRASS, FIRE): 0.7       # Grass is weak against Fire
 }
 
-# Define Player Class
+# Define Item class
+class Item:
+    def __init__(self, name, effect_type, effect_value, stackable=False, quantity=1):
+        self.name = name
+        self.effect_type = effect_type  # For example, 'heal', 'boost_attack', etc.
+        self.effect_value = effect_value
+        self.stackable = stackable
+        self.quantity = quantity
+
+    def use(self, pokemon):
+        if self.stackable:
+            if self.quantity > 0:
+                self.apply_effect(pokemon)
+                self.quantity -= 1
+                print(f"{pokemon.name} used {self.name}!")
+                if self.quantity == 0:
+                    print(f"There's no {self.name} left.")
+            else:
+                print(f"{self.name} is no longer available.")
+        else:
+            self.apply_effect(pokemon)
+            print(f"{pokemon.name} used {self.name}!")
+
+    def apply_effect(self, pokemon):
+        if self.effect_type == 'heal':
+            pokemon.health += self.effect_value
+            if pokemon.health > pokemon.health_cap:
+                pokemon.health = pokemon.health_cap  # Prevent health from exceeding the max limit.
+            print(f"{pokemon.name} healed for {self.effect_value} health!")
+        elif self.effect_type == 'boost_attack':
+            pokemon.damage += self.effect_value
+            print(f"{pokemon.name}'s attack increased by {self.effect_value}!")
+                  
+# Define Player class
 class Player():
     def __init__(self, name, gender):
         self.name = name
         self.gender = gender
+        self.inventory = []
 
+    def add_item(self, item):
+        self.inventory.append(item)
+        print(f"{item.name} has been added to your backpack.")
 
+    def use_item(self, item_name, pokemon):
+        for item in self.inventory:
+            if item.name == item_name:
+                item.use(pokemon)
+                return
+        print(f"{item_name} is not in your backpack.")
+
+    def display_inventory(self):
+        if not self.inventory:
+            print("Your backpack is empty.")
+        else:
+            print("Backpack:")
+            for item in self.inventory:
+                print(f"{item.name} - Quantity: {item.quantity}")
+    
 # Pokemon Class (for all Pokemon)
 class Pokemon():
     def __init__(self, name, health, health_cap, moves, element):
@@ -38,13 +100,13 @@ class Pokemon():
         self.element = element
 
     def get_elemental_multiplier(self, target_element):
-        if (self.element, target_element) in Element_Effectivness:
-            return Element_Effectivness[(self.element, target_element)]
-        elif (target_element, self.element) in Element_Effectivness:
-            return 1 / Element_Effectivness[(target_element, self.element)]
+        if (self.element, target_element) in element_effectiveness:
+            return element_effectiveness[(self.element, target_element)]
+        elif (target_element, self.element) in element_effectiveness:
+            return 1 / element_effectiveness[(target_element, self.element)]
         return 1
 
-    def Attack(self, target):
+    def attack(self, target):
         # Get the elemental multiplier (either amplifier or reducer)
         multiplier = self.get_elemental_multiplier(target.element)
         total_damage = self.damage * multiplier  # Calculate damage based on the multiplier
@@ -63,7 +125,7 @@ class Pokemon():
         print(f"{target.name}'s health is now {target.health:.2f}.")
         print(f"{self.name}'s health is {self.health:.2f}.")
 
-    def Run(self):                                                        ## Random chance to get away
+    def run(self):                                                        ## Random chance to get away
         Probability = (self.health_cap - self.health) / self.health_cap   ## The less health, the better chance to get away    
 
         if Probability > random.random():                                 ## random.random() returns random float between 1 and 0
@@ -73,7 +135,7 @@ class Pokemon():
             print(f"{self.name} couldn't get away\n")
             return False
     
-    def Fainted(self):
+    def fainted(self):
         if self.health <= 0:                           ## Check if Pokemon fainted
             self.health = 0
             print(f"{self.name} fainted")
@@ -94,37 +156,60 @@ Growlithe = Pokemon("Growlithe", 55,         {1:("Tackle", 30), 2:("Growl", 40)}
 Oddish = Pokemon("Oddish", 45, 45,           {1:("Tackle", 30), 2:("Vine Whip", 40)},  GRASS)
 Bellsprout = Pokemon("Bellsprout", 50, 50,   {1:("Tackle", 30), 2:("Vine Whip", 40)}, GRASS)
 
+# List of possible wild Pokemon that can appear
+wild_pokemon_list = [Bulbasaur, Squirtle, Charmander, Pikachu, Jigglypuff, Meowth, Psyduck, Eevee, Growlithe, Oddish, Bellsprout]
 
-def Battle(Player, Enemy):   ### Rework to call class methods for 
+def Battle(Player, Enemy):
+    """The battle function between the player and enemy Pokémon."""
     flag = True
     Turn = True
     while flag:
-        Decision = input("Attack or Run?")     ## Decide wheather to fight or run
-
-        if Decision.capitalize == "Attack":
-            Player.Attack(Enemy)               ## Use Attack from parent Character class
-            flag = not(Enemy.Fainted())
-            Turn = False
-
-        elif Decision.capitalize == "Run":     ## Use Run from parent Character class
-            flag = Player.Run()
-            Turn = False
-
-        else:
-            print("Incorrect Input, Try again")
+        # Player's turn
+        print("\nChoose an action:")
+        print("1) Attack")
+        print("2) Item")
+        print("3) Run")
         
-        if Turn == False & flag == True:                          ## Enemies turn
-            enemy_choice = random.random()     ##Random chance for enemy to Attack or run
+        try:
+            decision = int(input("Enter the number of the action you desire:"))  # Get the player's choice as a number
+        except ValueError:
+            print("Not an option. Enter a number from 1-3.")
+            continue
+        
+        if decision == 1:  # Attack
+            Player.Attack(Enemy)  # Use Attack method from Player's Pokemon
+            flag = not Enemy.Fainted()  # Check if the enemy has fainted
+            Turn = False
 
-            if enemy_choice > 0.90:            ## 10% chance to decide to run
-                flag = not Enemy.Run()
+        elif decision == 2:  # Item
+            # Show the player inventory and let the player choose an item to use
+            if not Player.items:
+                print("Your backpack is empty!")
+            else:
+                Player.display_inventory()  # Display all items in inventory
+                item_name = input("Which item do you want to use? ").capitalize()
+                Player.use_item(item_name, Enemy)  # Use item on the enemy or player’s Pokémon
+            Turn = False
+        
+        elif decision == 3:  # Run
+            flag = Player.Run()  # Use Run method from Player's Pokemon
+            Turn = False
+        
+        else:
+            print("Not an option, try again.")
+        
+        # Enemy's turn
+        if Turn == False and flag == True:  # Enemy's turn to attack
+            print(f"\n{Enemy.name}'s Turn:")
+            enemy_choice = random.random()  # Random chance for enemy to attack or run
+            
+            if enemy_choice > 0.95:  # 10% chance to decide to run
+                flag = not Enemy.Run()  # Enemy tries to run away
                 Turn = True
             else:
-                Enemy.Attack(Player)           ## 90% chance to attack
-                flag = not Player.Fainted()
+                Enemy.Attack(Player)  # 90% chance for enemy to attack
+                flag = not Pokemon.fainted()  # Check if player has fainted
                 Turn = True
-
-
 
 def TallGrass(Player):
     flag = True
@@ -132,7 +217,22 @@ def TallGrass(Player):
 
         Walk = input("Walk into the tall grass? (yes/no)")
 
-        if Walk.capitalize() == "Yes":
+        if Walk == "Yes":
+            # 10% chance to encounter a wild Pokemon
+            if random.random() > 0.9:  # 10% chance to encounter a Pokémon
+                # Randomly select a wild Pokémon
+                wild_pokemon = random.choice(wild_pokemon_list)
+                print(f"A wild {wild_pokemon.name} appeared!")
 
-            if random.random() > 0.9:            ## 10% chance per grass square to find pokemon
-                ##Pick random pokemon and battle
+                # Start a battle with the selected wild Pokémon
+                Battle(Player, wild_pokemon)
+
+                # Break out of the loop after the battle
+                break
+            else:
+                print("You didn't encounter any Pokémon this time.")
+        elif Walk == "No":
+            print("You chose not to walk into the tall grass.")
+            break  # Exit the loop if the player doesn't want to walk into the grass
+        else:
+            print("Please answer with 'yes' or 'no'.")
